3abeba6cca0da77ee65cad2fa169773bf4562916
atermanini <termanini@me.com>
2021-04-04 13:27:40 +0200

    changes

--- slurm/test1.sh
+++ slurm/test1.sh
@@ -12,4 +12,11 @@ echo "Job name: ${SLURM_JOB_NAME}, Job ID: ${SLURM_JOB_ID}"
 echo "Job submitted to the ${SLURM_JOB_PARTITION} partition, the default partition on ${SLURM_CLUSTER_NAME}"
 echo "I was launched from $SLURM_SUBMIT_DIR"
 echo "I have ${SLURM_CPUS_ON_NODE} CPUs on compute node $(hostname)"
-echo "I have ${SBATCH_TIMELIMIT} time to finish all my duties"
+
+sleep 20;
+echo "$(date)"
+echo "still here";
+
+sleep 20;
+echo "$(date)"
+echo "still here";
